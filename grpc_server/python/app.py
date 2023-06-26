c1ac37a369b44c3fb0beb6c0dfc62b64528a2118
RachhaAshwin <99497220+RachhaAshwin@users.noreply.github.com>
2023-06-26 14:57:49 -0400

    Update app.py

--- grpc_server/python/app.py
+++ grpc_server/python/app.py
@@ -13,51 +13,65 @@ stub = kv_pb2_grpc.ClientStub(channel)
 
 # Streamlit UI layout
 st.sidebar.title("gRPC Client")
-action = st.sidebar.selectbox("Select an action", ["Get", "Set", "List"],
-                              key="action_select")
+action = st.sidebar.selectbox("Select an action", ["Get", "Set", "List", "Upload Key-Value File"], key="action_select")
 st.sidebar.write("")
 
-
 def get_key():
-  key = st.text_input("Enter Key:", key="get_key_input")
-  if st.button("Get", key="get_button"):
-    try:
-      response = stub.Get(kv_pb2.GetKey(key=key))
-      if response.defined:
-        st.success(f"Value: {response.value}")
-      else:
-        st.warning("Key not found.")
-    except grpc.RpcError as e:
-      st.error(f"gRPC Error: {e.code().name} - {e.details()}")
-
+    key = st.text_input("Enter Key:", key="get_key_input")
+    if st.button("Get", key="get_button"):
+        try:
+            response = stub.Get(kv_pb2.GetKey(key=key))
+            if response.defined:
+                st.success(f"Value: {response.value}")
+            else:
+                st.warning("Key not found.")
+        except grpc.RpcError as e:
+            st.error(f"gRPC Error: {e.code().name} - {e.details()}")
 
 def set_key():
-  key = st.text_input("Enter Key:", key="set_key_input")
-  value = st.text_input("Enter Value:", key="set_value_input")
-  if st.button("Set", key="set_button"):
-    try:
-      stub.Set(kv_pb2.SetKey(key=key, value=value, broadcast=True))
-      st.success("Key-Value pair set successfully.")
-    except grpc.RpcError as e:
-      st.error(f"gRPC Error: {e.code().name} - {e.details()}")
-
+    key = st.text_input("Enter Key:", key="set_key_input")
+    value = st.text_input("Enter Value:", key="set_value_input")
+    if st.button("Set", key="set_button"):
+        try:
+            stub.Set(kv_pb2.SetKey(key=key, value=value, broadcast=True))
+            st.success("Key-Value pair set successfully.")
+        except grpc.RpcError as e:
+            st.error(f"gRPC Error: {e.code().name} - {e.details()}")
 
 def list_keys():
-  if st.button("List", key="list_button"):
-    try:
-      response = stub.List(kv_pb2.Void())
-      st.success("Key-value pairs defined on the server:")
-      for key, value in response.store.items():
-        st.write(f"- '{key}' = '{value}'")
-    except grpc.RpcError as e:
-      st.error(f"gRPC Error: {e.code().name} - {e.details()}")
+    if st.button("List", key="list_button"):
+        try:
+            response = stub.List(kv_pb2.Void())
+            st.success("Key-value pairs defined on the server:")
+            for key, value in response.store.items():
+                st.write(f"- '{key}' = '{value}'")
+        except grpc.RpcError as e:
+            st.error(f"gRPC Error: {e.code().name} - {e.details()}")
 
+def upload_file():
+    file = st.file_uploader("Upload Key-Value File (.txt)", type="txt", key="file_upload")
+    if file is not None:
+        content = file.read().decode("utf-8")
+        parse_and_display_key_value_pairs(content)
+
+def parse_and_display_key_value_pairs(content):
+    st.subheader("Parsed Key-Value Pairs:")
+    lines = content.splitlines()
+    for line in lines:
+        parts = line.split()
+        if len(parts) >= 2:
+            key = parts[0]
+            value = " ".join(parts[1:])
+            stub.Set(kv_pb2.SetKey(key=key, value=value, broadcast=True))
+            st.write(f"- '{key}': '{value}'")
 
 # Streamlit UI layout
 st.sidebar.title("gRPC Client")
 if action == "Get":
-  get_key()
+    get_key()
 elif action == "Set":
-  set_key()
+    set_key()
 elif action == "List":
-  list_keys()
+    list_keys()
+elif action == "Upload Key-Value File":
+    upload_file()
